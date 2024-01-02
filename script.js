2a93f990978571bec56e38b4d38579a7844dfcc2
Sour-abh-Raj <btech60064.21@bitmesra.ac.in>
2024-01-03 04:50:56 +0530

    api test

--- script.js
+++ script.js
@@ -1,28 +1,134 @@
+// PNR Status :
 document
-  .getElementById("statusForm")
+  .getElementById("PNRStatus")
   .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the form from submitting traditionally
+    event.preventDefault();
+    const PNR = document.getElementById("PNR").value;
+    const apiKey = "f88e34acc1msh774984c6e7cfb0ap10c327jsn4c29fcf2b260";
+    const rapidapiHost = "irctc1.p.rapidapi.com";
 
-    const trainNumber = document.getElementById("trainOrPnr").value;
-    // fetch(`https://www.railyatri.in/live-train-status/${trainNumber}`)
     fetch(
-      `https://www.railyatri.in/live-train-status/${trainNumber}?utm_source=lts_dweb_Check_status`
+      `https://irctc1.p.rapidapi.com/api/v3/getPNRStatus?pnrNumber=${PNR}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Rapidapi-Key": apiKey,
+          "X-Rapidapi-Host": rapidapiHost,
+        },
+      }
     )
       .then((response) => response.json())
       .then((data) => {
         // Display the fetched data in the result div
-        const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = JSON.stringify(data, null, 2);
-        resultDiv.style.border = "1px solid #000";
-        resultDiv.style.padding = "10px";
-        resultDiv.style.marginTop = "10px";
+        const resultDiv = document.getElementById("pnr-status-result");
+        // resultDiv.innerHTML = JSON.stringify(data, null, 2);
+        resultDiv.innerHTML = "";
+        data.forEach((element) => {
+          const stackDiv = document.createElement("div");
+          stackDiv.style.border = "1px solid #000";
+          stackDiv.style.padding = "10px";
+          stackDiv.style.marginTop = "10px";
+
+          for (const key in element) {
+            const keyElement = document.createElement("p");
+            keyElement.textContent = key;
+            stackDiv.appendChild(keyElement);
+
+            const valueElement = document.createElement("p");
+            valueElement.textContent = element[key];
+            stackDiv.appendChild(valueElement);
+          }
+
+          resultDiv.appendChild(stackDiv);
+        });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
         const resultDiv = document.getElementById("result");
         resultDiv.innerHTML = "Error fetching data. Please try again.";
+        resultDiv.style.border = "1px solid #000";
+        resultDiv.style.padding = "10px";
+        resultDiv.style.marginTop = "10px";
       });
   });
+
+// Train Status :
+document
+  .getElementById("trainStatus")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const trainNo = document.getElementById("trainNo").value;
+    const apiKey = "f88e34acc1msh774984c6e7cfb0ap10c327jsn4c29fcf2b260";
+    const rapidapiHost = "irctc1.p.rapidapi.com";
+
+    fetch(
+      `https://irctc1.p.rapidapi.com/api/v1/liveTrainStatus?trainNo=${trainNo}&startDay=1`,
+      {
+        method: "GET",
+        headers: {
+          "X-Rapidapi-Key": apiKey,
+          "X-Rapidapi-Host": rapidapiHost,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const resultDiv = document.getElementById("train-status-result");
+        resultDiv.innerHTML = "";
+        data.forEach((element) => {
+          const stackDiv = document.createElement("div");
+          stackDiv.style.border = "1px solid #000";
+          stackDiv.style.padding = "10px";
+          stackDiv.style.marginTop = "10px";
+
+          for (const key in element) {
+            const keyElement = document.createElement("p");
+            keyElement.textContent = key;
+            stackDiv.appendChild(keyElement);
+
+            const valueElement = document.createElement("p");
+            valueElement.textContent = element[key];
+            stackDiv.appendChild(valueElement);
+          }
+
+          resultDiv.appendChild(stackDiv);
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = "Error fetching data. Please try again.";
+        resultDiv.style.border = "1px solid #000";
+        resultDiv.style.padding = "10px";
+        resultDiv.style.marginTop = "10px";
+      });
+  });
+
+// document
+//   .getElementById("statusForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevents the form from submitting traditionally
+
+//     const trainNumber = document.getElementById("trainOrPnr").value;
+//     // fetch(`https://www.railyatri.in/live-train-status/${trainNumber}`)
+//     fetch(
+//       `https://www.railyatri.in/live-train-status/${trainNumber}?utm_source=lts_dweb_Check_status`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         // Display the fetched data in the result div
+//         const resultDiv = document.getElementById("result");
+//         resultDiv.innerHTML = JSON.stringify(data,statusForm null, 2);
+//         resultDiv.style.border = "1px solid #000";
+//         resultDiv.style.padding = "10px";
+//         resultDiv.style.marginTop = "10px";
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//         const resultDiv = document.getElementById("result");
+//         resultDiv.innerHTML = "Error fetching data. Please try again.";
+//       });
+//   });
 
 // document
 //   .getElementById("statusForm")
